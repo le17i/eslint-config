8d020c8882598b008d565fb158e202858a97c755
Leandro Dias <ledias.17@gmail.com>
2022-08-30 14:40:48 -0300

    chore: update settings to eslint

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
     sourceType: "module",
   },
   plugins: ["@typescript-eslint/eslint-plugin"],
@@ -14,10 +13,10 @@ module.exports = {
   ],
   root: true,
   env: {
+    browser: true,
     node: true,
     jest: true,
   },
-  ignorePatterns: [".eslintrc.js"],
   rules: {
     "@typescript-eslint/interface-name-prefix": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
